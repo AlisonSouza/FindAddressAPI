d5bf3446ab64ddb09ee510504e11e13188aaf893
Alison Rodrigues de Souza <alisons@dbserver.com.br>
2017-06-10 22:44:39 -0300

    First Integration Test to validate the router

---
+++ src/test/java/com/bb/find/address/ApplicationTest.java
@@ -0,0 +1,39 @@
+package com.bb.find.address;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.google.code.geocoder.model.GeocodeResponse;
+import com.google.code.geocoder.model.GeocoderStatus;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ApplicationTest {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void shouldReturnAmsterdamLocation() {
+		ResponseEntity<GeocodeResponse> response = restTemplate.exchange("/camel/geocoder?address=Amsterdam",
+				HttpMethod.GET, null, new ParameterizedTypeReference<GeocodeResponse>() {
+				});
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		GeocodeResponse res = response.getBody();
+		assertThat(res.getStatus()).isEqualTo(GeocoderStatus.OK);
+		assertThat(res.getResults()).isNotEmpty();
+		assertThat(res.getResults()).element(0).hasFieldOrPropertyWithValue("formattedAddress",
+				"Amsterdam, Netherlands");
+	}
+}
